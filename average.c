3e1a7a5d4650bc94c5cbc063a852d560665831fd
niteshjangid007 <nitesh242007@gmail.com>
2025-10-05 15:01:32 +0530

    average of three number

---
+++ average.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (){
+ int x;
+    printf("entre the number :");
+        scanf("%d", &x);
+           int y;
+    printf("entre the number :");
+        scanf("%d", &y);
+        int z;
+    printf("entre the number :");
+        scanf("%d", &z);
+
+        printf("%d", (x+y+z)/3);
+    return 0;
+}
